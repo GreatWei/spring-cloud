00c73ba533031e777f5d2166282ebb6e0adcdcb7
GreatWei <1121412775@qq.com>
2018-05-06 01:16:07 +0800

    D

--- service-eureka/src/main/java/com/example/serviceeureka/ServiceEurekaApplication.java
+++ service-eureka/src/main/java/com/example/serviceeureka/ServiceEurekaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceEurekaApplication {
-/*test1*/
+/*test11*/
     public static void main(String[] args) {
         SpringApplication.run(ServiceEurekaApplication.class, args);
     }
