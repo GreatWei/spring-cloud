d699f4e9f55a738bd4fa7271595caf2929210bf4
GreatWei <1121412775@qq.com>
2018-05-07 00:51:21 +0800

    hystrix

---
+++ HystrixDashboard/src/main/java/com/example/hystrix/HystrixApplication.java
@@ -0,0 +1,12 @@
+package com.example.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+}
