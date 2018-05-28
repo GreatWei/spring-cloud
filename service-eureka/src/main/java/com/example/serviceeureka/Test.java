7681f80e0ffd89684ceb3db3b2baad2533f24645
GreatWei <1121412775@qq.com>
2018-05-28 23:29:01 +0800

    server-config

---
+++ service-eureka/src/main/java/com/example/serviceeureka/Test.java
@@ -0,0 +1,13 @@
+package com.example.serviceeureka;
+
+public class Test  implements  TestInter{
+    @Override
+    public void forTest() {
+
+    }
+
+    private  String name;
+
+
+
+}
---
+++ service-eureka/src/main/java/com/example/serviceeureka/TestInter.java
@@ -0,0 +1,5 @@
+package com.example.serviceeureka;
+
+public interface TestInter {
+    public void forTest();
+}
