7050018db8cb1713e18e1337b582e13a465432ba
GreatWei <1121412775@qq.com>
2018-06-07 00:08:25 +0800

    rabbitmq
    Signed-off-by: GreatWei <1121412775@qq.com>

--- server-config-client/src/main/java/com/example/serverconfigclient/ServerConfigClientApplication.java
+++ server-config-client/src/main/java/com/example/serverconfigclient/ServerConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.example.serverconfigclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-
+@RefreshScope
 public class ServerConfigClientApplication {
 
     public static void main(String[] args) {
