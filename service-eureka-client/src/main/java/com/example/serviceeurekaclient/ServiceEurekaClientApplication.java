a02f5840dc2efa27300634afd4dfaa44a88c43b0
GreatWei <1121412775@qq.com>
2018-07-09 17:22:49 +0800

    Hystrix

--- service-eureka-client/src/main/java/com/example/serviceeurekaclient/ServiceEurekaClientApplication.java
+++ service-eureka-client/src/main/java/com/example/serviceeurekaclient/ServiceEurekaClientApplication.java
@@ -1,10 +1,13 @@
 package com.example.serviceeurekaclient;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,5 +17,16 @@ public class ServiceEurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceEurekaClientApplication.class, args);
+
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
@@ -1,7 +1,9 @@
 package com.example.serviceribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -24,4 +26,14 @@ public class ServiceRibbonApplication {
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+//    @Bean
+//    public ServletRegistrationBean getServlet() {
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
 }
